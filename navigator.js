60bd95a6a6de38acfea8a06f913b9aa9a5f439ce
Abdurahman <manabdurahman2007@gmail.com>
2026-02-16 22:31:52 +0300

    create logic

---
+++ navigator.js
@@ -0,0 +1,8 @@
+let addressLat = 10;
+let addressLong = 20;
+
+let positionLat = 2;
+let positionLong = 5;
+
+console.log("Разница по широте (lat): " + (addressLat - positionLat));
+console.log("Разница по долготе (long): " + (addressLong - positionLong));
